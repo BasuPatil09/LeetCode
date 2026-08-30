8227dddd093a285a76923cbcef3bd3650d7458ae
BASU PATIL <om2580sai@gmail.com>
2026-08-30 19:50:48 +0530

    Time: 0 ms (100%), Space: 15 MB (40.58%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) return {};
+        vector<int> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                auto node = q.front(); q.pop();
+                if (size == 0) ans.push_back(node->val);
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+        return ans;
+    }
+};
